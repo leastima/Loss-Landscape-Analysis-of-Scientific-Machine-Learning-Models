6904ec03a8629b6987600f9e86fd9e9800e087d9
StephenW830 <xpwang830@gmail.com>
2025-07-12 09:54:13 +0800

    Add files via upload

--- cka.py
+++ cka.py
@@ -1,21 +1,14 @@
-"""Run PINNs for convection/reaction/reaction-diffusion with periodic boundary conditions."""
-
+import wandb
 import argparse
-from net_pbc import *
-import numpy as np
-import os
-import random
+import sys
+import traceback
 import torch
-from systems_pbc import *
-import torch.backends.cudnn as cudnn
-from utils import *
-from visualize import *
-import matplotlib.pyplot as plt
-import copy
-from sklearn.manifold import MDS
-from pyhessian import *
-
+import os
+import numpy as np
 import math
+
+from src.train_utils import *
+from src.models import PINN
 
 class CKA(object):
     def __init__(self):
@@ -63,172 +56,91 @@ class CKA(object):
 
         return hsic / (var1 * var2)
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pde', type=str,
+                        default='convection', help='PDE type')
+    parser.add_argument('--pde_params', nargs='+', type=str,
+                        default=None, help='PDE coefficients')
+    parser.add_argument('--num_layers', type=int, default=4,
+                        help='number of layers of the neural net')
+    parser.add_argument('--num_neurons', type=int, default=50,
+                        help='number of neurons per layer')
+    parser.add_argument('--loss', type=str, default='mse',
+                        help='type of loss function')
+    parser.add_argument('--num_x', type=int, default=257,
+                        help='number of spatial sample points (power of 2 + 1)')
+    parser.add_argument('--num_t', type=int, default=101,
+                        help='number of temporal sample points')
+    parser.add_argument('--num_res', type=int, default=10000,
+                        help='number of sampled residual points')
+    parser.add_argument('--wandb_project', type=str,
+                        default='pinns', help='W&B project name')
+    parser.add_argument('--device', type=str, default=0, help='GPU to use')
+    parser.add_argument('--save_path', type=str, help='path to save the results of experiments')
 
-################
-# Arguments
-################
-parser = argparse.ArgumentParser(description='Characterizing/Rethinking PINNs')
+    # Extract arguments from parser
+    args = parser.parse_args()
 
-parser.add_argument('--system', type=str, default='convection', help='System to study.')
-parser.add_argument('--seed', type=int, default=0, help='Random initialization.')
-parser.add_argument('--collocation_seed', type=int, default=0, help='Random initialization.')
+    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
 
-parser.add_argument('--N_f', type=int, default=100, help='Number of collocation points to sample.')
-parser.add_argument('--optimizer_name', type=str, default='LBFGS', help='Optimizer of choice.')
-parser.add_argument('--lr', type=float, default=1.0, help='Learning rate.')
-parser.add_argument('--L', type=float, default=1.0, help='Multiplier on loss f.')
+    print("Collocation points: ", args.num_res)
+    print("PDE coefficient: ", args.pde_params[1])
 
-parser.add_argument('--xgrid', type=int, default=256, help='Number of points in the xgrid.')
-parser.add_argument('--nt', type=int, default=100, help='Number of points in the tgrid.')
-parser.add_argument('--nu', type=float, default=1.0, help='nu value that scales the d^2u/dx^2 term. 0 if only doing advection.')
-parser.add_argument('--rho', type=float, default=1.0, help='reaction coefficient for u*(1-u) term.')
-parser.add_argument('--beta', type=float, default=1.0, help='beta value that scales the du/dx term. 0 if only doing diffusion.')
-parser.add_argument('--u0_str', default='sin(x)', help='str argument for initial condition if no forcing term.')
-parser.add_argument('--source', default=0, type=float, help="If there's a source term, define it here. For now, just constant force terms.")
+    model_list = []
+    if args.pde == "convection":
+        model_folder = os.path.join("saved_models", "system_convection", f"N_f_{args.num_res}", f"beta_{args.pde_params[1]}")
+    elif args.pde == "reaction":
+        model_folder = os.path.join("saved_models", "system_reaction", f"N_f_{args.num_res}", f"rho_{args.pde_params[1]}")
 
-parser.add_argument('--layers', type=str, default='50,50,50,50,1', help='Dimensions/layers of the NN, minus the first layer.')
-parser.add_argument('--net', type=str, default='DNN', help='The net architecture that is to be used.')
-parser.add_argument('--activation', default='tanh', help='Activation to use in the network.')
-parser.add_argument('--loss_style', default='mean', help='Loss for the network (MSE, vs. summing).')
+    for filename in os.listdir(model_folder):
+        if filename.endswith(".pt"):
+            path = os.path.join(model_folder, filename)
+            model = PINN(in_dim=2, hidden_dim=args.num_neurons, out_dim=1, num_layer=args.num_layers).to(device)
+            model.load_state_dict(torch.load(path))
+            model.eval()
+            model_list.append(model)
 
-parser.add_argument('--visualize', default=False, help='Visualize the solution.')
-parser.add_argument('--save_model', default=False, help='Save the model for analysis later.')
+    x_range, t_range, loss_func, pde_coefs = get_pde(args.pde, args.pde_params, args.loss)
+    n_x_test = int((args.num_x - 1) / 2) + 1
+    n_t_test = args.num_t
+    x, t, data_params = get_data(x_range, t_range, n_x_test, n_t_test, random=False, device=device)
 
-parser.add_argument('--save_path', type=str, help='path to save results.')
-parser.add_argument('--model_path', type=str, help='path to load models to evaluate.')
-parser.add_argument('--model_seeds', type=str, nargs='+', help='list of model seeds to evaluate.')
+    metrics = {}
+    
+    # calculate the CKA similarity
+    linear_cka_matrix = np.zeros((len(model_list), len(model_list)))
+    for i in range(len(model_list)):
+        for j in range(len(model_list)):
+            if j <= i:
+                linear_cka_matrix[i, j] = 0
+            else:
+                model0 = model_list[i]
+                model1 = model_list[j]
 
-args = parser.parse_args()
+                X = torch.vstack(predict(x, t, model0)).cpu().detach().numpy()
+                Y = torch.vstack(predict(x, t, model1)).cpu().detach().numpy()
 
-# CUDA support
+                X = torch.tensor(X).float().to(device)
+                Y = torch.tensor(Y).float().to(device)
 
-device = torch.device('cuda')
+                np_cka = CKA()
+                cka_res = np_cka.linear_CKA(X, Y)
+                linear_cka_matrix[i, j] = cka_res
 
-nu = args.nu
-beta = args.beta
-rho = args.rho
+    metrics['linear_cka_matrix'] = linear_cka_matrix
+    print("The CKA matrix is:")
+    print(linear_cka_matrix)
 
-if args.system == 'diffusion': # just diffusion
-    beta = 0.0
-    rho = 0.0
-elif args.system == 'convection':
-    nu = 0.0
-    rho = 0.0
-elif args.system == 'rd': # reaction-diffusion
-    beta = 0.0
-elif args.system == 'reaction':
-    nu = 0.0
-    beta = 0.0
+    if args.pde == "convection":
+        folder = os.path.join(args.save_path, "system_convection", f"N_f_{args.num_res}", f"beta_{float(args.pde_params[1])}")
+    elif args.pde == "reaction":
+        folder = os.path.join(args.save_path, "system_reaction", f"N_f_{args.num_res}", f"rho_{float(args.pde_params[1])}")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        np.save(folder + '/cka_metric.npy', metrics)
+    else:
+        np.save(folder + '/cka_metric.npy', metrics)
 
-print('nu', nu, 'beta', beta, 'rho', rho)
-
-# parse the layers list here
-orig_layers = args.layers
-layers = [int(item) for item in args.layers.split(',')]
-
-############################
-# Process data
-############################
-
-x = np.linspace(0, 2*np.pi, args.xgrid, endpoint=False).reshape(-1, 1) # not inclusive
-t = np.linspace(0, 1, args.nt).reshape(-1, 1)
-X, T = np.meshgrid(x, t) # all the X grid points T times, all the T grid points X times
-X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None])) # all the x,t "test" data
-
-# remove initial and boundaty data from X_star
-t_noinitial = t[1:]
-# remove boundary at x=0
-x_noboundary = x[1:]
-X_noboundary, T_noinitial = np.meshgrid(x_noboundary, t_noinitial)
-X_star_noinitial_noboundary = np.hstack((X_noboundary.flatten()[:, None], T_noinitial.flatten()[:, None]))
-
-# sample collocation points only from the interior (where the PDE is enforced)
-X_f_train = sample_random(X_star_noinitial_noboundary, args.N_f, seed=args.collocation_seed)
-
-if 'convection' in args.system or 'diffusion' in args.system:
-    u_vals = convection_diffusion(args.u0_str, nu, beta, args.source, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-elif 'rd' in args.system:
-    u_vals = reaction_diffusion_discrete_solution(args.u0_str, nu, rho, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-elif 'reaction' in args.system:
-    u_vals = reaction_solution(args.u0_str, rho, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-else:
-    print("WARNING: System is not specified.")
-
-u_star = u_vals.reshape(-1, 1) # Exact solution reshaped into (n, 1)
-Exact = u_star.reshape(len(t), len(x)) # Exact on the (x,t) grid
-
-xx1 = np.hstack((X[0:1,:].T, T[0:1,:].T)) # initial condition, from x = [-end, +end] and t=0
-uu1 = Exact[0:1,:].T # u(x, t) at t=0
-bc_lb = np.hstack((X[:,0:1], T[:,0:1])) # boundary condition at x = 0, and t = [0, 1]
-uu2 = Exact[:,0:1] # u(-end, t)
-
-# generate the other BC, now at x=2pi
-t = np.linspace(0, 1, args.nt).reshape(-1, 1)
-x_bc_ub = np.array([2*np.pi]*t.shape[0]).reshape(-1, 1)
-bc_ub = np.hstack((x_bc_ub, t))
-
-u_train = uu1 # just the initial condition
-X_u_train = xx1 # (x,t) for initial condition
-
-layers.insert(0, X_u_train.shape[-1])
-
-############################
-# Train the model
-############################
-
-set_seed(args.seed) # for weight initialization
-
-model = PhysicsInformedNN_pbc(args.system, X_u_train, u_train, X_f_train, bc_lb, bc_ub, layers, G, nu, beta, rho,
-                            args.optimizer_name, args.lr, args.net, args.L, args.activation, args.loss_style)
-# model.train()
-
-
-metrics = {}
-
-
-
-# for CKA similarity
-# model 
-model_list = []
-for seed in args.model_seeds:
-    path = f"{args.model_path}/seed_{seed}.pt"
-
-
-    model = torch.load(path, map_location=device)
-    model.dnn.eval()
-    if torch.is_grad_enabled():
-        model.optimizer.zero_grad()
-    model_list.append(copy.deepcopy(model))
-
-
-# calculate the CKA similarity
-linear_cka_matrix = np.zeros((len(model_list), len(model_list)))
-for i in args.model_seeds:
-    for j in args.model_seeds:
-        if j <= i:
-            linear_cka_matrix[i, j] = 0
-        else:
-            model0 = model_list[i]
-            model1 = model_list[j]
-
-            X = model0.predict(X_star)
-            Y = model1.predict(X_star)
-            print('predict finished')
-
-            X = torch.tensor(X).float().to(device)
-            Y = torch.tensor(Y).float().to(device)
-
-            np_cka = CKA()
-            cka_res = np_cka.linear_CKA(X, Y)
-            linear_cka_matrix[i, j] = cka_res
-            print('measure finished')
-
-metrics['linear_cka_matrix'] = linear_cka_matrix
-
-
-if not os.path.exists(args.save_path):
-    os.makedirs(args.save_path)
-np.save(args.save_path + '/cka_metric.npy', metrics)
+if __name__ == "__main__":
+    main()
--- mode_connectivity.py
+++ mode_connectivity.py
@@ -1,237 +1,142 @@
-"""Run PINNs for convection/reaction/reaction-diffusion with periodic boundary conditions."""
-
+import wandb
 import argparse
-from net_pbc import *
-import numpy as np
-import os
-import random
+import sys
+import traceback
 import torch
-from systems_pbc import *
-import torch.backends.cudnn as cudnn
-from utils import *
-from visualize import *
-import matplotlib.pyplot as plt
-import copy
-from sklearn.manifold import MDS
-from pyhessian import *
-
+import os
+import numpy as np
 import math
+import copy
 
+from src.train_utils import *
+from src.models import PINN
+from collections import OrderedDict
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pde', type=str,
+                        default='convection', help='PDE type')
+    parser.add_argument('--pde_params', nargs='+', type=str,
+                        default=None, help='PDE coefficients')
+    parser.add_argument('--num_layers', type=int, default=4,
+                        help='number of layers of the neural net')
+    parser.add_argument('--num_neurons', type=int, default=50,
+                        help='number of neurons per layer')
+    parser.add_argument('--loss', type=str, default='mse',
+                        help='type of loss function')
+    parser.add_argument('--num_x', type=int, default=257,
+                        help='number of spatial sample points (power of 2 + 1)')
+    parser.add_argument('--num_t', type=int, default=101,
+                        help='number of temporal sample points')
+    parser.add_argument('--num_res', type=int, default=10000,
+                        help='number of sampled residual points')
+    parser.add_argument('--epochs', type=int, default=1000,
+                        help='number of epochs to run')
+    parser.add_argument('--wandb_project', type=str,
+                        default='pinns', help='W&B project name')
+    parser.add_argument('--device', type=str, default=0, help='GPU to use')
+    parser.add_argument('--save_path', type=str, help='path to save the results of experiments')
 
-################
-# Arguments
-################
-parser = argparse.ArgumentParser(description='Characterizing/Rethinking PINNs')
+    # Extract arguments from parser
+    args = parser.parse_args()
 
-parser.add_argument('--system', type=str, default='convection', help='System to study.')
-parser.add_argument('--seed', type=int, default=0, help='Random initialization.')
-parser.add_argument('--collocation_seed', type=int, default=0, help='Random initialization.')
+    device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
 
-parser.add_argument('--N_f', type=int, default=100, help='Number of collocation points to sample.')
-parser.add_argument('--optimizer_name', type=str, default='LBFGS', help='Optimizer of choice.')
-parser.add_argument('--lr', type=float, default=1.0, help='Learning rate.')
-parser.add_argument('--L', type=float, default=1.0, help='Multiplier on loss f.')
+    model_list = []
+    if args.pde == "convection":
+        model_folder = os.path.join("saved_models", "system_convection", f"N_f_{args.num_res}", f"beta_{args.pde_params[1]}")
+        dataset_path = os.path.join("./dataset", f'system_{experiment_args["pde"]}', 
+                          f'N_f_{experiment_args["num_res"]}',f'beta_{float(experiment_args["pde_params"][-1])}', 'train.pt') 
 
-parser.add_argument('--xgrid', type=int, default=256, help='Number of points in the xgrid.')
-parser.add_argument('--nt', type=int, default=100, help='Number of points in the tgrid.')
-parser.add_argument('--nu', type=float, default=1.0, help='nu value that scales the d^2u/dx^2 term. 0 if only doing advection.')
-parser.add_argument('--rho', type=float, default=1.0, help='reaction coefficient for u*(1-u) term.')
-parser.add_argument('--beta', type=float, default=1.0, help='beta value that scales the du/dx term. 0 if only doing diffusion.')
-parser.add_argument('--u0_str', default='sin(x)', help='str argument for initial condition if no forcing term.')
-parser.add_argument('--source', default=0, type=float, help="If there's a source term, define it here. For now, just constant force terms.")
+    for filename in os.listdir(model_folder):
+        if filename.endswith(".pt"):
+            path = os.path.join(model_folder, filename)
+            model = PINN(in_dim=2, hidden_dim=args.num_neurons, out_dim=1, num_layer=args.num_layers).to(device)
+            model.load_state_dict(torch.load(path))
+            model.eval()
+            model_list.append(model)
 
-parser.add_argument('--layers', type=str, default='50,50,50,50,1', help='Dimensions/layers of the NN, minus the first layer.')
-parser.add_argument('--net', type=str, default='DNN', help='The net architecture that is to be used.')
-parser.add_argument('--activation', default='tanh', help='Activation to use in the network.')
-parser.add_argument('--loss_style', default='mean', help='Loss for the network (MSE, vs. summing).')
+    # calculate the LMC
+    lmc_matrix = {}
+    for i in range(len(model_list)):
+        lmc_matrix[i] = {}
+        for j in range(len(model_list)):
+            lmc_matrix[i][j] = {}
 
-parser.add_argument('--visualize', default=False, help='Visualize the solution.')
-parser.add_argument('--save_model', default=False, help='Save the model for analysis later.')
+            model1 = model_list[i].state_dict()
+            model2 = model_list[j].state_dict()
+            new_state_dict = OrderedDict()
 
-parser.add_argument('--save_path', type=str, help='path to save results.')
-parser.add_argument('--model_path', type=str, help='path to load models to evaluate.')
-parser.add_argument('--model_seeds', type=str, nargs='+', help='list of model seeds to evaluate.')
+            error_list = []
+            loss_list = []
+            for alpha in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+                for key in model1.keys():
+                    if key in model2:
+                        new_state_dict[key] = alpha * model1[key] + (1 - alpha) * model2[key]
+                    else:
+                        raise ValueError(f"Key '{key}' not found in model2")
+                    
+                model = PINN(in_dim=2, hidden_dim=args.num_neurons, out_dim=1, num_layer=args.num_layers).to(device)
+                model.load_state_dict(new_state_dict)
+                model.eval()
 
-args = parser.parse_args()
+                x_range, t_range, loss_func, pde_coefs = get_pde(args.pde, args.pde_params, args.loss)
+                data = torch.load(dataset_path, map_location=device)
+                x = (data['x_res'], data['x_left'], data['x_upper'], data['x_lower'])
+                t = (data['t_res'], data['t_left'], data['t_upper'], data['t_lower'])
+                data_params = data['data_params']
+            
+                loss_res, loss_bc, loss_ic = loss_func(x, t, predict(x, t, model))
+                loss = loss_res + loss_bc + loss_ic
+                loss_list.append(loss.item())
 
-# CUDA support
+                n_x_test = int((args.num_x - 1) / 2) + 1
+                n_t_test = args.num_t
+                x_test, t_test, data_params_test = get_data(x_range, t_range, n_x_test, 
+                                                        n_t_test, random=False, device=device)
 
-device = torch.device('cuda')
+                with torch.no_grad():
+                    predictions = torch.vstack(predict(x_test, t_test, model)).cpu().detach().numpy()
+                targets = get_ref_solutions(args.pde, pde_coefs, x_test, t_test, data_params_test)
+                test_l2re = l2_relative_error(predictions, targets)
+                error_list.append(test_l2re)
 
-nu = args.nu
-beta = args.beta
-rho = args.rho
+            lmc_matrix[i][j]['loss'] = loss_list
+            lmc_matrix[i][j]['error'] = error_list
 
-if args.system == 'diffusion': # just diffusion
-    beta = 0.0
-    rho = 0.0
-elif args.system == 'convection':
-    nu = 0.0
-    rho = 0.0
-elif args.system == 'rd': # reaction-diffusion
-    beta = 0.0
-elif args.system == 'reaction':
-    nu = 0.0
-    beta = 0.0
+    mc = np.zeros((len(model_list), len(model_list)))
 
-print('nu', nu, 'beta', beta, 'rho', rho)
+    for i in range(len(model_list)):
+        for j in range(len(model_list)):
+            if j <= i:
+                mc[i][j] = 0
+            
+            else:
+                loss_list = lmc_matrix[i][j]['loss']  # loss list alpha from 0 to 1
+        
+                L_theta = loss_list[-1]  # alpha=1.0
+                L_theta_prime = loss_list[0]  # alpha=0.0
+        
+                mid_losses = np.array(loss_list)
+                midpoint = 0.5 * (L_theta + L_theta_prime)
+        
+                deviations = np.abs(midpoint - mid_losses)
+                t_star_idx = np.argmax(deviations)
+                L_gamma_t_star = mid_losses[t_star_idx]
+        
+                mc_val = midpoint - L_gamma_t_star
+                mc[i][j] = mc_val
 
-# parse the layers list here
-orig_layers = args.layers
-layers = [int(item) for item in args.layers.split(',')]
+    print("The mc matrix is:")
+    print(mc)
 
-############################
-# Process data
-############################
+    if args.pde == "convection":
+        folder = os.path.join(args.save_path, "system_convection", f"N_f_{args.num_res}", f"beta_{float(args.pde_params[1])}")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        np.save(folder + '/mode_connectivity.npy', mc)
+    else:
+        np.save(folder + '/mode_connectivity.npy', mc)
 
-x = np.linspace(0, 2*np.pi, args.xgrid, endpoint=False).reshape(-1, 1) # not inclusive
-t = np.linspace(0, 1, args.nt).reshape(-1, 1)
-X, T = np.meshgrid(x, t) # all the X grid points T times, all the T grid points X times
-X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None])) # all the x,t "test" data
-
-# remove initial and boundaty data from X_star
-t_noinitial = t[1:]
-# remove boundary at x=0
-x_noboundary = x[1:]
-X_noboundary, T_noinitial = np.meshgrid(x_noboundary, t_noinitial)
-X_star_noinitial_noboundary = np.hstack((X_noboundary.flatten()[:, None], T_noinitial.flatten()[:, None]))
-
-# sample collocation points only from the interior (where the PDE is enforced)
-X_f_train = sample_random(X_star_noinitial_noboundary, args.N_f, seed=args.collocation_seed)
-
-if 'convection' in args.system or 'diffusion' in args.system:
-    u_vals = convection_diffusion(args.u0_str, nu, beta, args.source, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-elif 'rd' in args.system:
-    u_vals = reaction_diffusion_discrete_solution(args.u0_str, nu, rho, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-elif 'reaction' in args.system:
-    u_vals = reaction_solution(args.u0_str, rho, args.xgrid, args.nt)
-    G = np.full(X_f_train.shape[0], float(args.source))
-else:
-    print("WARNING: System is not specified.")
-
-u_star = u_vals.reshape(-1, 1) # Exact solution reshaped into (n, 1)
-Exact = u_star.reshape(len(t), len(x)) # Exact on the (x,t) grid
-
-xx1 = np.hstack((X[0:1,:].T, T[0:1,:].T)) # initial condition, from x = [-end, +end] and t=0
-uu1 = Exact[0:1,:].T # u(x, t) at t=0
-bc_lb = np.hstack((X[:,0:1], T[:,0:1])) # boundary condition at x = 0, and t = [0, 1]
-uu2 = Exact[:,0:1] # u(-end, t)
-
-# generate the other BC, now at x=2pi
-t = np.linspace(0, 1, args.nt).reshape(-1, 1)
-x_bc_ub = np.array([2*np.pi]*t.shape[0]).reshape(-1, 1)
-bc_ub = np.hstack((x_bc_ub, t))
-
-u_train = uu1 # just the initial condition
-X_u_train = xx1 # (x,t) for initial condition
-
-layers.insert(0, X_u_train.shape[-1])
-
-############################
-# Train the model
-############################
-
-set_seed(args.seed) # for weight initialization
-
-model = PhysicsInformedNN_pbc(args.system, X_u_train, u_train, X_f_train, bc_lb, bc_ub, layers, G, nu, beta, rho,
-                            args.optimizer_name, args.lr, args.net, args.L, args.activation, args.loss_style)
-# model.train()
-
-
-metrics = {}
-
-
-
-
-
-# model 
-path = f"{args.model_path}/seed_0.pt"
-
-model_base = torch.load(path, map_location=device)
-
-model_list = []
-for seed in args.model_seeds:
-    path = f"{args.model_path}/seed_{seed}.pt"
-
-    model = torch.load(path, map_location=device)
-    model.dnn.eval()
-    if torch.is_grad_enabled():
-        model.optimizer.zero_grad()
-    model_list.append(copy.deepcopy(model.dnn))
-
-
-# calculate the LMC
-lmc_matrix = {}
-for i in range(len(model_list)):
-    lmc_matrix[i] = {}
-    for j in range(len(model_list)):
-        lmc_matrix[i][j] = {}
-
-        model1 = model_list[i].state_dict()
-        model2 = model_list[j].state_dict()
-        # 初始化新 checkpoint 的状态字典
-        new_state_dict = OrderedDict()
-
-        error_list = []
-        loss_list = []
-        for alpha in [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]:
-            # 计算线性加权平均
-            for key in model1.keys():
-                # 确保键在两个模型中一致
-                if key in model2:
-                    new_state_dict[key] = alpha * model1[key] + (1 - alpha) * model2[key]
-                else:
-                    raise ValueError(f"Key '{key}' not found in model2")
-
-            model = copy.deepcopy(model_base)
-            model.dnn.load_state_dict(new_state_dict)
-            model.dnn.eval()
-
-
-            # calculate the hessian eigenvalues and eigenvectors
-            lmc_x_u = torch.tensor(X_u_train[:, 0:1], requires_grad=True).float().to(device)
-            lmc_t_u = torch.tensor(X_u_train[:, 1:2], requires_grad=True).float().to(device)
-            lmc_u = torch.tensor(u_train, requires_grad=True).float().to(device)
-
-            lmc_x_f = torch.tensor(X_f_train[:, 0:1], requires_grad=True).float().to(device)
-            lmc_t_f = torch.tensor(X_f_train[:, 1:2], requires_grad=True).float().to(device)
-
-            lmc_x_bc_lb = torch.tensor(bc_lb[:, 0:1], requires_grad=True).float().to(device)
-            lmc_t_bc_lb = torch.tensor(bc_lb[:, 1:2], requires_grad=True).float().to(device)
-            lmc_x_bc_ub = torch.tensor(bc_ub[:, 0:1], requires_grad=True).float().to(device)
-            lmc_t_bc_ub = torch.tensor(bc_ub[:, 1:2], requires_grad=True).float().to(device)
-
-            if torch.is_grad_enabled():
-                model.optimizer.zero_grad()
-                
-            u_pred = model.net_u(lmc_x_u, lmc_t_u)
-            u_pred_lb = model.net_u(lmc_x_bc_lb, lmc_t_bc_lb)
-            u_pred_ub = model.net_u(lmc_x_bc_ub, lmc_t_bc_ub)
-            if nu != 0:
-                u_pred_lb_x, u_pred_ub_x = model.net_b_derivatives(u_pred_lb, u_pred_ub, lmc_x_bc_lb, lmc_x_bc_ub)
-            f_pred = model.net_f(lmc_x_f, lmc_t_f)
-
-            loss_u = torch.mean((lmc_u - u_pred) ** 2)
-            loss_b = torch.mean((u_pred_lb - u_pred_ub) ** 2)
-            if nu != 0:
-                loss_b += torch.mean((u_pred_lb_x - u_pred_ub_x) ** 2)
-            loss_f = torch.mean(f_pred ** 2)
-
-            loss = loss_u + loss_b + args.L*loss_f
-            loss_list.append(loss.item())
-
-
-            # for test error
-            u_pred = model.predict(X_star)
-            error_u_relative = np.linalg.norm(u_star-u_pred, 2)/np.linalg.norm(u_star, 2)
-            error_list.append(error_u_relative)
-
-        lmc_matrix[i][j]['loss'] = loss_list
-
-
-if not os.path.exists(args.save_path):
-    os.makedirs(args.save_path)
-np.save(args.save_path + '/cka_metric.npy', metrics)
+if __name__ == "__main__":
+    main()
